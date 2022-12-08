7d8ad6db6ecd3829a854b027eb82c757aede7534
AgonBllaca-Litera <agon.bllaca@litera.com>
2022-12-08 22:29:57 +0100

    Modified get handler to serve views/index.html

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
+const path = require('path');
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
 
 app.get('/',(req,res)=>{
-    res.send("Hello Express")
+    res.sendFile(path.join(__dirname,'views','index.html'));
 })
 
 
